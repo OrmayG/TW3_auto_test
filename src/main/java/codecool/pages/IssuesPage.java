94275c942fcfd6e26d5cb273dfd3958e2a1f6940
OrmayG <113803159+OrmayG@users.noreply.github.com>
2024-02-13 10:28:19 +0100

    added IssuesPage class

---
+++ src/main/java/codecool/pages/IssuesPage.java
@@ -0,0 +1,26 @@
+
+package codecool.pages;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class IssuesPage{
+
+    WebDriver driver;
+    
+    @FindBy(className = "search-button")
+    WebElement searchButton;
+
+
+
+
+    public IssuesPage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver,this);    
+    }
+
+    public void clickSearchButton(){
+        searchButton.click();     
+    }
+}
