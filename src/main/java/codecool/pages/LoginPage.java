da3604a26a70efb21a070c180a9ad126e800959e
PeterH <hegyespeter05@gmail.com>
2024-02-14 15:39:07 +0100

    finish refactoring login tests

--- src/main/java/codecool/pages/LoginPage.java
+++ src/main/java/codecool/pages/LoginPage.java
@@ -4,18 +4,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage {
-    WebDriver driver;
-    WebDriverWait wait;
-    String URL = "https://jira-auto.codecool.metastage.net/secure/Dashboard.jspa";
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private String URL = "https://jira-auto.codecool.metastage.net/secure/Dashboard.jspa";
     @FindBy(id ="login-form-username")
-    WebElement userNameInput;
+    private WebElement userNameInput;
     @FindBy(id ="login-form-password")
-    WebElement userPasswordInput;
+    private WebElement userPasswordInput;
     @FindBy(id ="login")
-    WebElement loginButton;
+    private WebElement loginButton;
+    @FindBy(css = "#gadget-0-title")
+    private WebElement loginGadget;
 
     public LoginPage(WebDriver driver,WebDriverWait wait) {
         this.driver = driver;
@@ -27,7 +30,7 @@ public class LoginPage {
     public void login(String username, String password){
         setUsername(username);
         setPassword(password);
-        login();
+        pressLoginButton();
     }
     private void setUsername(String username){
         userNameInput.sendKeys(username);
@@ -35,7 +38,12 @@ public class LoginPage {
     private void setPassword(String password){
         userPasswordInput.sendKeys(password);
     }
-    private void login(){
+    public void pressLoginButton(){
+        wait.until(ExpectedConditions.visibilityOf(loginButton));
         loginButton.click();
+    }
+
+    public boolean isUserOnLoginScreen() {
+        return loginGadget.isDisplayed();
     }
 }
