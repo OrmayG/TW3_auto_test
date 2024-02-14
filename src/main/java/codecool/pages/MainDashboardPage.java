a7f1fb78649e81f3e5fe2d11cf9d1e3387ef3471
OrmayG <113803159+OrmayG@users.noreply.github.com>
2024-02-14 09:39:59 +0100

    typos

--- src/main/java/codecool/pages/MainDashboardPage.java
+++ src/main/java/codecool/pages/MainDashboardPage.java
@@ -1,13 +1,41 @@
 package codecool.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.time.temporal.TemporalUnit;
+import java.util.concurrent.TimeUnit;
 
 public class MainDashboardPage {
     WebDriver driver;
+    WebDriverWait wait;
 
-    public MainDashboardPage(WebDriver driver) {
+    @FindBy(id="issues_new_search_link")
+    WebElement issuesButton;
+    @FindBy(id="find_link_content")
+    WebElement issuesTab;
+    @FindBy(id = "create_link")
+    WebElement createNewIssueButton;
+    public MainDashboardPage(WebDriver driver, WebDriverWait wait) {
         this.driver = driver;
+        this.wait = wait;
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("create_link")));
         PageFactory.initElements(driver,this);
+    }
+
+    public void navigateToIssuesPage(){
+        issuesButton.click();
+        issuesTab.click();
+    }
+    public void openNewIssueWindow(){
+
+        createNewIssueButton.click();
     }
 }
--- src/test/java/IssueIsPresent.java
+++ src/test/java/IssueIsPresent.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class IssueIsPresent {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class IssueIsPresent {
+    WebDriver driver = new FirefoxDriver();
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+
 }
